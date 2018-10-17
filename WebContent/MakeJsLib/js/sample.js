2aff7125493085ebc4cd7acc711ec8f6e13274f0
winch <wincheran@gmail.com>
2018-10-18 01:55:14 +0900

    ajax 코드 추가

--- WebContent/MakeJsLib/js/sample.js
+++ WebContent/MakeJsLib/js/sample.js
@@ -5,7 +5,7 @@
 window.onload = function() {
 }
 
-// 통신 관련 객체
+// 통신 관련 객체 (Javascript)
 var Communication = {
 	/**
 	 * get 방식을 사용하여 서버에 데이터 요청하기.
@@ -42,3 +42,58 @@ var Communication = {
 		request.send(msg); // 요청 본문을 msg로 전송한다.
 	} 
 }
+
+// 통신 관련 객체 (jQuery)
+var jCommunication = {
+	/**
+	 * oParam : ajax 요청에 필요한 옵션을 가진 객체
+	 */
+	ajax: function(oParam){
+		// 필수 옵션 존재 여부 체크 메서드
+		function existAjaxDefaultOption(oParam){
+			var aDefaultOptionName = ['type', 'url', 'data', 'dataType', 'success'];
+			var aTemp = [];
+			Object.keys(oParam).forEach(function(key){
+				// 해당 키가 디폴트 옵션이면서, 추가된 배열(aTemp)에 중복이 없으면
+				if($.inArray(key, aDefaultOptionName) !== -1 && $.inArray(key, aTemp) === -1){
+					aTemp.push(key);
+				}
+			});
+			return true;
+		}
+	
+		// Ajax 요청에 필요한 옵션 객체 얻는 메서드
+		function getAjaxOptionObject(oParam){
+			var aOptionName = [
+				'type', 'url', 'data', 'dataType', 'success'
+			]; // todo 추가적으로 옵션명 넣어 줘야 함. (많아서 패스..)
+			
+			var oTemp = {};
+			oTemp['type'] = oParam['type'];
+			Object.keys(aOptionName).forEach(function(key){
+				oTemp[aOptionName[key]] = oParam[aOptionName[key]];
+			})
+			return oTemp;
+		}
+		
+		if(!existAjaxDefaultOption(oParam)){
+			return false;
+		}
+		
+		oParam = getAjaxOptionObject(oParam);
+		 
+		// 3. ajax 요청.
+		$.ajax(oDefaultParam);
+	},
+
+}
+
+
+
+
+
+
+
+
+
+
